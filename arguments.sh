91eb21b74cca1e5bcff6d09848e630c15fac8370
gautami4 <gowthami.it4@gmail.com>
2026-07-07 16:28:58 +0530

    arguments

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2 #arguments are run time variables
+
+echo "$PERSON1 :: hi $PERSON2  "
+echo "$PERSON2 :: hi $PERSON1 "
+echo "$PERSON1  :: how are u $PERSON2 "
+
