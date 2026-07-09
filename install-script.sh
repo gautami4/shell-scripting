0145efa9f6e263e80ac01d8a758f02179e34ca0c
gautami4 <gowthami.it4@gmail.com>
2026-07-09 11:48:06 +0530

    install script

--- install-script.sh
+++ install-script.sh
@@ -6,6 +6,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "You should have sudo access to execute the script"
+    exit 1 #give any number other than zero
 fi    
 
 dnf install mysqll -y
