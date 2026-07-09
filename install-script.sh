f59ba54305312669500366d77ce931de54b1446e
gautami4 <gowthami.it4@gmail.com>
2026-07-09 12:16:39 +0530

    install script

--- install-script.sh
+++ install-script.sh
@@ -7,24 +7,38 @@ if [ $USERID -ne 0 ]
 then
     echo "You should have sudo access to execute the script"
     exit 1 #give any number other than zero
-fi    
+fi 
 
-dnf install mysql -y
- 
+dnf list installed mysql
+
 if [ $? -ne 0 ]
 then
-    echo "Installing MYSQL ...Failure" 
-    exit 1
+    dnf install mysql -y
+
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MYSQL ...Failure" 
+        exit 1
+    else
+        echo "Installing MYSQL ...success" 
+    fi 
 else
-    echo "Installing MYSQL ...success" 
-fi   
+    echo "MYSQL already installed"
+fi          
 
-dnf install git -y
+dnf list installed git
 
-if [ $? -ne 0 ]
+if [ $? -ne 0]
 then
-    echo "Installing GIT ...Failue"
-    exit 1
- else
-    echo "Installing Git success"
- fi      
+    dnf install git -y
+
+    if [ $? -ne 0 ]
+    then
+        echo "Installing GIT ...Failue"
+        exit 1
+    else
+        echo "Installing Git success"
+    fi 
+else
+    echo "Git already installed"
+fi             
