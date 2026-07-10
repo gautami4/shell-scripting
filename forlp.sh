05cec8b5ac07f36f626a4a7a7442cd149b1104a2
gautami4 <gowthami.it4@gmail.com>
2026-07-10 19:05:33 +0530

    forlp

---
+++ forlp.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# writing a new program to install packages if not installed already
+
+# getting the user id 
+USERID=$(id -u)
+
+# write a function to do check whether package already installed
+
+installchk(){
+    if [ $1 -eq 0 ]; then
+        echo  "$packages ... is already installed"    
+    else
+        dnf install $packages -y
+        valchk $? $packages
+    fi
+ 
+}
+
+# write a function to do install packages
+valchk(){
+    
+        if [ $1 -eq 0 ]; then
+            echo "$packages .. installed successfully"
+            
+        else
+            
+            echo "$packages .. installation failed"
+        fi
+}   
+
+
+if [ $USERID -ne 0 ] ;then
+    echo  "You are not root user so you dont have authority to install"
+    exit 1
+else
+    for packages in $@ 
+        dnf list installed $packages  
+        installchk $? $packages
+    done
+fi
+
