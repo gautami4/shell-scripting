5035e57fd60ade8d9a10c16bebd68175f8f8ceb7
gautami4 <gowthami.it4@gmail.com>
2026-07-10 15:29:47 +0530

    no hard code

---
+++ nohardcode.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#check if root user is logged in
+
+USERROOT=$(id -u)
+
+#function to check if pack installed or not
+VALPACK(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 ...  success"
+    else
+        echo "$2 ... failure"
+    fi
+}
+
+
+if [ $USERROOT -ne 0  ]
+then
+    echo "You are not a root user"
+    exit 1
+fi
+
+for packagess in $@
+do
+    dnf list installed $packagess
+
+    if [ $? -ne 0 ]
+    then
+        dnf list install $packagess -y
+        
+        VALPACK $? "Installing $packagess"
+
+
+done
