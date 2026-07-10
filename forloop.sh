f9eaa05c6604fd5f54c693dd6f43fb02adae1b80
gautami4 <gowthami.it4@gmail.com>
2026-07-10 13:55:40 +0530

    forloop

---
+++ forloop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# writing a new program to install packages if not installed already
+
+# getting the user id 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ] ;then
+  echo  "You are not root user so you dont have authority to install"
+  exit 1
+fi
+
+Validate(){
+    dnf list installed mysql 
+    if [ $1 -eq 0 ] ; then
+        echo  " $2 ..... already installed"
+        exit 1
+    else
+        dnf install mysql -y
+        if [ $1 -eq 0 ] ; then
+            echo "$2 ..... installed successfully"
+        else
+            echo "$2 ..... installation failed"
+        fi
+    fi
+}
+# check whether package is already istalled
+if [ $? -eq 0 ] ;then
+    Validate $? MYSQL
+fi
