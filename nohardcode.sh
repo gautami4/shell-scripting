2586f5aacdadf44f5bf31e777e106327ef46a72e
gautami4 <gowthami.it4@gmail.com>
2026-07-11 10:29:25 +0530

    pf

--- nohardcode.sh
+++ nohardcode.sh
@@ -25,21 +25,21 @@ if [ $USERROOT -ne 0  ]
 then
     echo "You are not a root user"
     exit 1
-fi
-
-for packagess in $@
-do
-    dnf list installed $packagess
-    echo " status is : $? $packagess" 
+else
+    for packagess in $@
+    do
+        dnf list installed $packagess
+        echo " status is : $? $packagess" 
     
-    if [ $? -ne 0 ]
-    then
-        dnf list install $packagess -y  &>>$LOGFILENAME
+         if  [ $? -ne 0 ]
+        then
+            dnf list install $packagess -y  &>>$LOGFILENAME
         
-        VALPACK $? "Installing $packagess"
+              VALPACK $? "Installing $packagess"
 
-    else 
-        echo " $packagess is already Installed" 
-    fi    
+        else 
+            echo " $packagess is already Installed" 
+         fi    
 
-done
+    done
+fi
