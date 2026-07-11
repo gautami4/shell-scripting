03e81212c3363f7e12e1f1d1c68847f1e930bc32
gautami4 <gowthami.it4@gmail.com>
2026-07-11 09:34:05 +0530

    log

---
+++ newlogprgm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+#check if user have root access
+if [ $? -ne 0 ]
+then
+    echo "you should have root access to run this script"
+fi 
+
+if [ $? -eq 0 ]
+then
+    dnf list installed nginx
+fi
+
+if [ $? -eq 0 ]
+then
+    echo "nginx already installed"
+else
+    dnf install nginx
+
+    if [ $? -eq 0 ]
+    then
+        echo "nginx installation success"
+    else
+        echo "ngix installation failure"
+    fi        
+fi
+            
+
